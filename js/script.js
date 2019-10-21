ce7bb47f0a08147e8291d3039ea6fc975f25fee5
ndpatel5173 <npatel8745.student@hsfi.us>
2019-10-21 22:07:52 +0000

    10/21/2019. I was attempting to output a value, but failed.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("#button").click(function(){
+function firstCharacter(string){
+    var suffix = string.substring(0,1);
+    return suffix + "ay";
+}
+console.log(firstCharacter);
+$("button").click(() => {
+    let firstCharacter = $("input").val();
+    $("body").append(firstCharacter);
+})
 
-
-
-  
-  
-  
-  
 });
