73190b643009ea522e5d366f3bd0d4de24b2def3
ndpatel5173 <npatel8745.student@hsfi.us>
2019-10-27 20:50:36 +0000

    10/27/2019 I attempted to have the output appear of the page, but failed.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
-$("#button").click(function(){
 function firstCharacter(string){
-    var suffix = string.substring(0,1);
-    return suffix + "ay";
+  var suffix = string.substring(0,1);
+  return suffix + "ay";
 }
-console.log(firstCharacter);
-$("button").click(() => {
-    let firstCharacter = $("input").val();
-    $("body").append(firstCharacter);
-})
+console.log(firstCharacter(input));
+
+function allButfirstCharacter(string){
+  var pig = string.substring(1,string.length);
+  //var pig = string.substring(1,Infinity);
+  return pig;
+}
+console.log(allButfirstCharacter(input));
+$("button").click(function(){
+    $(".input").val();
 
 });
+console.log(typeof allButfirstCharacter + firstCharacter);
+})
