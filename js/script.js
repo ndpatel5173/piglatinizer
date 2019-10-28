56ee7c6f8f118fbe7f2a66b33eb8f7646100d3a3
ndpatel5173 <npatel8745.student@hsfi.us>
2019-10-28 22:09:28 +0000

    10/28/2019 appeared on website, have to add the rest of the word before suffix

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
-function firstCharacter(string){
-  var suffix = string.substring(0,1);
-  return suffix + "ay";
-}
-console.log(firstCharacter(input));
-
-function allButfirstCharacter(string){
-  var pig = string.substring(1,string.length);
-  //var pig = string.substring(1,Infinity);
-  return pig;
-}
-console.log(allButfirstCharacter(input));
+//console.log(allButfirstCharacter(input));
 $("button").click(function(){
-    $(".input").val();
-
+    let word = $(".input").val();
+    var suffix = word.substring(0,1);
+        $("#button").click(function(){
+        $(".output").text(suffix + "ay");
+    });
+console.log(word);
+console.log(suffix);
 });
-console.log(typeof allButfirstCharacter + firstCharacter);
+//console.log(typeof allButfirstCharacter + firstCharacter);
+//function firstCharacter(string){
+  //var suffix = string.substring(0,1);
+  //$("#button").click(function(){
+    //  $(".output").text(suffix + "ay");
+  //});
+ // return suffix + "ay";
+//}
+console.log(firstCharacter(suffix));
+
+//function allButfirstCharacter(string){
+  //var pig = string.substring(1,string.length);
+  //var pig = string.substring(1,Infinity);
+  //return pig;
+//}
 })
